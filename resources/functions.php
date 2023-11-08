5df1efcfea977c01b21db304a2bb501671339734
Adnan <adnan@Adnans-MBP.home>
2023-11-08 18:30:54 +0100

    missing files

---
+++ resources/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+function redirect($location)
+{
+    header("Location: $location");
+}
+
+function query($sql)
+{
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result)
+{
+    global $connection;
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_errno($connection));
+    }
+}
+
+function escape_string($string)
+{
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result)
+{
+    return mysqli_fetch_array($result);
+}
